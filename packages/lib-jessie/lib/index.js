c438cfeedaff5f23b3225d6c2e76c365bdec64f1
Michael FIG <mfig@agoric.com>
2020-05-22 01:04:02 -0600

    feat: add protect/suspect and local harden to lib-jessie

--- packages/lib-jessie/lib/index.js
+++ packages/lib-jessie/lib/index.js
@@ -5,6 +5,7 @@
 export { confine, confineExpr } from './confine.js';
 export { makePromise, makeMap, makeSet, makeWeakMap, makeWeakSet } from './makers.js';
 export { default as harden } from '@lib-jessie/harden';
+export { protect, suspect } from '@lib-jessie/zones';
 
 // FIXME: Need to remove insulate entirely!
 export const insulate = o => o;
