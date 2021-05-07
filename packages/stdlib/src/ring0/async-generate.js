6a4ef3bf9851e670fcf80e29f521afc5a90875c5
Michael FIG <mfig@agoric.com>
2021-05-06 21:22:41 -0600

    chore: fix some missing details

--- packages/stdlib/src/ring0/async-generate.js
+++ packages/stdlib/src/ring0/async-generate.js
@@ -1,11 +1,14 @@
+// @ts-check
+
 /**
  * Return an async iterable that produces iterator results via `next`.
  *
  * This works around Jessie's forbiddance of Symbol.
  *
  * @template T,TReturn
- * @param {() => IteratorResult<T, TReturn> | Promise<IteratorResult<T,
- * TReturn>>} next produce an iterator result
+ * @param {() => IteratorResult<T, TReturn> |
+ *               Promise<IteratorResult<T, TReturn>>
+ * } next produce a single-step iterator result
  * @returns {AsyncIterable<T>}
  */
 const asyncGenerate = next => {
