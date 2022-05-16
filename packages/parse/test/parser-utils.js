cf92a52cab339ffb28720dd2cdbcbc94f72a9fdc
Michael FIG <mfig@agoric.com>
2022-05-15 19:38:51 -0600

    test(parse): implement JSON, Justin tests

---
+++ packages/parse/test/parser-utils.js
@@ -0,0 +1,40 @@
+/// <reference path="../src/peg.d.ts"/>
+import * as util from 'util';
+import tagString from '../src/tag-string.js';
+
+let curSrc = '';
+
+/**
+ * @param {IParserTag<any>} tag
+ */
+export function makeParser(tag) {
+  /** @type {IParserTag<any>} */
+  const stringTag = tagString(tag);
+  /**
+   * @param {string} src
+   * @param {boolean} [doDump]
+   * @param {boolean} [doDebug]
+   */
+  return (src, doDump = false, doDebug = false) => {
+    curSrc = src;
+    const dtag = doDebug ? stringTag.options({ debug: true }) : stringTag;
+    const parsed = dtag`${src}`;
+    if (doDump) {
+      // tslint:disable-next-line:no-console
+      console.log('Dump:', util.inspect(parsed, { depth: Infinity }));
+      doDump = false;
+    }
+    return parsed;
+  };
+}
+
+/**
+ *
+ * @param {number} pos
+ * @param  {...unknown} args
+ */
+export function ast(pos, ...args) {
+  return Object.assign(args, {
+    _pegPosition: `${JSON.stringify(curSrc[pos])} #0:${pos}`,
+  });
+}
