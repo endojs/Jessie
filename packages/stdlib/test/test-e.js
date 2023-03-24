a594ff9a8217aafe896936fcda5f482155fb3b60
Turadg Aleahmad <turadg@agoric.com>
2023-03-24 08:42:10 -0700

    test: remove dependence on agoric-sdk

--- packages/stdlib/test/test-e.js
+++ packages/stdlib/test/test-e.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { test } from '@agoric/swingset-vat/tools/prepare-test-env-ava.js';
+import '@endo/init/debug.js';
+import test from 'ava';
 
 import { E } from '../src/ring0/E.js';
 
